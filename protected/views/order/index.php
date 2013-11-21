9b6b5b57a6a963e0ce14ccafb5e099550e0d8200
Vova Lysenko <svoop@bk.ru>
2013-11-21 11:22:11 +0200

    Корректное отображение способов оплаты на странице оформления заказа.

--- protected/views/order/index.php
+++ protected/views/order/index.php
@@ -7,12 +7,13 @@
         <? if(!empty($delivery)):?>
             <? $count = count($delivery);?>
             <? foreach($delivery as $key=>$value):?>
-                <li data-id="<?= $value->id;?>" data-type="<?= $value->Type;?>" <?=$key==$count-1?'class="fl"':''?> data-price='<?= Calculate::DiscountDelivery($order['totalSum'],$value->Price,$value->FreeIf);?>' data-name='<?= $value->Name;?>'>
+                <li data-id="<?= $value->id;?>" data-type="<?= $value->Type;?>" <?=($key+1)%3 == 0?'class="fl"':''?> data-price='<?= Calculate::DiscountDelivery($order['totalSum'],$value->Price,$value->FreeIf);?>' data-name='<?= $value->Name;?>'>
                     <input type="radio"><p><?= $value->Name;?>:</p><?= $value->Instruction;?><br>Стоимость: <?= round($value->Price);?> Р
                 </li>
             <? endforeach;?>
         <? endif;?>
         </ul>
+        <div class="clearfix"></div>
     </div>
 
     <div class="delivery-payment b-payment">
@@ -24,11 +25,12 @@
                         <ul>
                             <? $count = count($value->payment_api);?>
                             <? foreach($value->payment_api as $key=>$deliverypayment):?>
-                                <li data-id="<?= $deliverypayment->id;?>" <?=$key==$count-1?'class="fl"':''?> data-name='<?= $deliverypayment->Name;?>'>
+                                <li data-id="<?= $deliverypayment->id;?>" <?=($key+1)%3 == 0?'class="fl"':''?> data-name='<?= $deliverypayment->Name;?>'>
                                     <input type="radio"><p><?= $deliverypayment->Name;?>:</p><?= $deliverypayment->ShortDescription;?>
                                 </li>
                             <? endforeach;?>
                         </ul>
+                        <div class="clearfix"></div>
                     </div>
                 <? endif;?>
             <? endforeach;?>
